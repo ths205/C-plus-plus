30941bd7eb9dc6ed9a6fbe2c069a6892953bed9b
Tiffany Schiff <ths205@alum.lehigh.edu>
2020-04-17 15:08:20 -0400

    Create quartile_cal.cpp

---
+++ quartile_cal.cpp
@@ -0,0 +1,77 @@
+//Calculates the first,second and third Quartiles of an array
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    cin>>n;
+
+    if( !cin || n<=0)
+    {
+        cout<<"You either gave bad input or an integer that is 0 or less.\n";
+        exit(0);
+
+    }
+
+    int x[n];
+
+    for( int i=0; i<n; i++)
+    {
+        cin>>x[i];
+        
+        if(!cin)
+        {
+            cout<<"You gave bad input.\n";
+            exit(0);
+
+        }
+    }
+    //Sort the array
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++){
+
+            if(x[i] > x[j])
+            {
+                int temp=x[i];
+                x[i]=x[j];
+                x[j]=temp;
+            }
+        }
+    }
+    
+    //if the number of integers in an array is odd
+    if(n%2 != 0)
+    {
+        int q2 = x[(n/2)];
+        int q1 = (x[(n/4)-1]+x[(n/4)])/2;
+        int q3 = (x[(3*n/4)]+x[(3*n/4)+1])/2;
+
+        cout<<q1<<"\n";
+        cout<<q2<<"\n";
+        cout<<q3<<"\n";
+    }
+
+    //if the number of integers in an array is even and if the number of integers that make up half the array are odd
+    else if (n%2==0 && n%4!=0) {
+       int q2 = (x[(n/2)-1]+x[(n/2)])/2;
+       int q1 = (x[(n/4)]);
+       int q3 = (x[(3*n/4)]);
+       cout<<q1<<"\n";
+        cout<<q2<<"\n";
+        cout<<q3<<"\n";
+    }
+   
+    //if the number of integers in an array is even and if the number of integers that make up half the array are odd
+    else{
+        int q2 =(x[(n/2)-1]+x[(n/2)])/2;
+        int q1= (x[(n/4)-1]+x[(n/4)])/2;
+        int q3 = (x[((3*n)/4)-1]+x[(3*n/4)])/2;
+        cout<<q1<<"\n";
